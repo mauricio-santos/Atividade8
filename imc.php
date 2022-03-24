be97e626caebc1ee80fd1e09382edc5214f04575
Maurício Vieira dos Santos <mauricio.santos@web.de>
2022-03-24 19:41:09 +0100

    Logica IMC adicionada

--- imc.php
+++ imc.php
@@ -1,3 +1,31 @@
+<?php
+	if($_SERVER['REQUEST_METHOD']==='POST'){
+		
+		$arr["nome"] = filter_input(INPUT_POST,'nome',FILTER_SANITIZE_SPECIAL_CHARS);
+		$arr["altura"] = filter_input(INPUT_POST,'altura',FILTER_SANITIZE_SPECIAL_CHARS);
+		$arr["peso"] = filter_input(INPUT_POST,'peso',FILTER_SANITIZE_SPECIAL_CHARS);
+		
+		$imc["valor"]= $arr["peso"]/$arr["altura"]*$arr["altura"];
+		
+		if($imc["valor"]<18.5){
+			$imc["classificacao"]="MAGREZA";
+			$imc["grau"]="0";
+		}elseif($imc["valor"]<24.9){
+			$imc["classificacao"]="NORMAL";
+			$imc["grau"]="0";
+		}elseif($imc["valor"]<29.9){
+			$imc["classificacao"]="SOBREPESO";
+			$imc["grau"]="I";
+		}elseif($imc["valor"]<39.9){
+			$imc["classificacao"]="OBESIDADE";
+			$imc["grau"]="II";
+		}else{
+			$imc["classificacao"]="OBESIDADE GRAVE";
+			$imc["grau"]="III";
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
